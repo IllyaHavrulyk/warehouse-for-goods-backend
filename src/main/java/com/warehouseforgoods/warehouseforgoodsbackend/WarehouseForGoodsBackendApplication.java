d968845bb9a103b51e51c984aa1ce246a0629288
Illya Havrulyk <peoso595@gmail.com>
2021-02-20 18:23:23 +0200

    Initial Commit

---
+++ src/main/java/com/warehouseforgoods/warehouseforgoodsbackend/WarehouseForGoodsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.warehouseforgoods.warehouseforgoodsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseForGoodsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseForGoodsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/warehouseforgoods/warehouseforgoodsbackend/WarehouseForGoodsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehouseforgoods.warehouseforgoodsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseForGoodsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
