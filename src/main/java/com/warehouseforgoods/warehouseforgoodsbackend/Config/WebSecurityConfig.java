8a4de11f9bae2cd44b14796c37e7fe103971c998
Illya Havrulyk <peoso595@gmail.com>
2021-04-13 23:24:32 +0300

    resolved login error

--- src/main/java/com/warehouseforgoods/warehouseforgoodsbackend/Config/WebSecurityConfig.java
+++ src/main/java/com/warehouseforgoods/warehouseforgoodsbackend/Config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.warehouseforgoods.warehouseforgoodsbackend.Config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
@@ -19,49 +24,64 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-   private final UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
-   @Autowired
-   public WebSecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
-      this.userDetailsService = userDetailsService;
-   }
+    @Autowired
+    public WebSecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
-   @Override
-   protected void configure(HttpSecurity http) throws Exception{
-       http.cors().and().csrf().disable()
-               .authorizeRequests()
-               .antMatchers("/user/list","/registration").permitAll()
-               .anyRequest()
-               .authenticated()
-               .and()
-               .httpBasic()
-               .and()
-               .formLogin().loginPage("/login").permitAll()
-               .and()
-               .logout()
-               .logoutRequestMatcher(new AntPathRequestMatcher("/logout","POST"))
-               .invalidateHttpSession(true)
-               .clearAuthentication(true)
-               .deleteCookies("JSESSIONID");
-   }
-   @Override
-   protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder){
-      authenticationManagerBuilder.authenticationProvider(daoAuthenticationProvider());
-   }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/list", "/registration").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.BAD_REQUEST))
 
-   @Bean
-   protected DaoAuthenticationProvider daoAuthenticationProvider(){
-      DaoAuthenticationProvider daoAuthenticationProvider= new DaoAuthenticationProvider();
-      daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-      daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin().failureHandler(authenticationFailureHandler()).loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID");
+    }
 
-      return daoAuthenticationProvider;
-   }
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler(){
+        return new CustomAccessDeniedHandler();
+    }
 
-   @Bean
-   protected PasswordEncoder passwordEncoder(){
-      return new BCryptPasswordEncoder(12);
-   }
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new CustomAuthenticationFailureHandler();
+    }
+
+    @Bean
+    protected DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 
 
 }
